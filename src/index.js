422d469e4ee2280abcf53933236e982592429583
Adams-Ghub <bamfoadamsf@gmail.com>
2023-04-18 15:14:38 +0000

    Create the basic structure of project and styled it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
+const DATA = [
+  {
+    name: 'Player one',
+    score: '300',
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+  },
+  {
+    name: 'Player two',
+    score: '300',
 
-    return element;
-}
+  },
+  {
+    name: 'Player three',
+    score: '300',
 
-document.body.appendChild(component());
+  },
+  {
+    name: 'Player four',
+    score: '300',
+
+  },
+];
+
+const scoreboard = document.querySelector('.score-board');
+
+const showScore = () => {
+  for (let i = 0; i < DATA.length; i++) {
+    const boardItem = document.createElement('div');
+    const itemName = document.createElement('span');
+    const itemScore = document.createElement('span');
+
+    if (i % 2 > 0) {
+      boardItem.id = 'odd';
+    }
+    boardItem.classList.add('score-item');
+    itemName.textContent = `${DATA[i].name}: `;
+    itemScore.textContent = DATA[i].score;
+    boardItem.appendChild(itemName);
+    boardItem.appendChild(itemScore);
+    scoreboard.appendChild(boardItem);
+  }
+};
+
+window.addEventListener('load', () => {
+  showScore();
+});
